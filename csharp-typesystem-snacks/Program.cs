4d502d48d7047cf9586eefde4bc397033f47bd71
KatiaLosignore <katialos.kl@gmail.com>
2023-09-06 14:25:17 +0200

    Aggiungere i file di progetto.

---
+++ csharp-typesystem-snacks/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+// Snack 1: L’utente inserisce due numeri in successione. Il software stampa il maggiore.
+
+Console.WriteLine("Inserisci il primo numero:");
+
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inserisci il secondo numero:");
+
+int numero2 = int.Parse(Console.ReadLine());
+
+if (numero1 > numero2)
+{
+    Console.WriteLine($"Il primo numero è maggiore: {numero1}");
+} else if (numero1 == numero2)
+{
+    Console.WriteLine("I due numeri sono uguali!");
+} else 
+{
+    Console.WriteLine($"Il secondo numero è maggiore: {numero2}");
+}
